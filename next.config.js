252dd0fd5283982c548fb1bff0220530a07437f3
sauravkalaskar <skalaska@asu.edu>
2024-12-10 23:40:45 -0700

    Remove experimental appDir configuration and enable client components in page.tsx

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['api.mapbox.com'],
     formats: ['image/webp'],
